9d4e54760b6adf3b7b76dce7ff46132e1253bc59
Felipe Sá <felipesa01@gmail.com>
2023-08-10 20:38:57 -0300

    End of section 4

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+
+    }
+
+    public class BaseController
+    {
+    }
+}
